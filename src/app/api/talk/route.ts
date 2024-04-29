8be601ca4f946e8960af066041d55b09e7c0afc3
mehmettberke <dxd3d9@gmail.com>
2024-04-29 21:12:35 +0300

    outpuy

--- src/app/api/talk/route.ts
+++ src/app/api/talk/route.ts
@@ -28,6 +28,7 @@ export async function POST(req: Request) {
     "cjwbw/sadtalker:3aa3dac9353cc4d6bd62a8f95957bd844003b401ca4e4a9b33baa574c549d376",
     { input }
   );
-
+  console.log("output");
+  console.log(output);
   return new Response(JSON.stringify({ url: output }));
 }
