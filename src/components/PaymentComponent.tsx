27ffaf5b9bfad0ca9f1d3cb0dec454dcc56ff4af
mehmettberke <dxd3d9@gmail.com>
2024-05-30 16:59:36 +0300

    live mode

--- src/components/PaymentComponent.tsx
+++ src/components/PaymentComponent.tsx
@@ -91,7 +91,7 @@ const PaymentComponent = ({
   return (
     <div>
       <Script
-        src="https://www.paypal.com/sdk/js?client-id=ATk9UOlq4DNa7YJ8wfl4s4lBeiNYCIgNYa8-xntjbHDRakQG89bTE4O_87i61fMB3pBY3A-qKUWZYowA&enable-funding=venmo&currency=USD"
+        src="https://www.paypal.com/sdk/js?client-id=AaUOHHqnJaZmJOgfzyf35eCvM3ktkXo-jBUZr6kGBK5dTPtGyQFY0hxfvJO-296FcSGi2l__dS6_W30w&enable-funding=venmo&currency=USD"
         onLoad={() => {
           console.log("PayPal script loaded");
           setIsScriptLoaded(true);
